ecee1a601fea58865de868afbde24b1137fb609e
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-05-07 15:48:56 -0300

    fix test

--- tests/evaluator.spec.js
+++ tests/evaluator.spec.js
@@ -9,20 +9,20 @@ describe('Evaluator', () => {
 
     const evaluatorFile = buildPath(pwd, 'evaluator.js');
     const jestOutputFile = buildPath(pwd, 'tests/jest-output.json');
-    const requirementsMappingFile = buildPath(pwd, 'tests/requirements_mapping.json');
+    const requirementsMappingFile = buildPath(pwd, 'tests/requirements.json');
     const resultFile = buildPath(pwd, 'tests/result.json');
 
     const expectedResultJson = {
       github_username: 'no_actor',
       github_repository_name: 'no_repository',
       evaluations: [{
-        requirement_id: 1,
-        grade: 3,
+        description: "Sum module",
+        grade: 3
       }, {
-        requirement_id: 2,
-        grade: 1
+        description: "Power module",
+        grade: 3
       }, {
-        requirement_id: 3,
+        description: "Multiply module",
         grade: 1
       }]
     }
