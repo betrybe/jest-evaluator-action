62d3cd8fb794254a38c4d2318b2684737feee7d7
igorgbr <igorgphotoarte@gmail.com>
2022-05-11 09:41:13 -0300

    teste com evaluator teste na raiz

---
+++ evaluator.spec.js
@@ -0,0 +1,56 @@
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+const buildPath = (absolutePath, filePath) => (`${absolutePath}/${filePath}`);
+
+describe('Evaluator', () => {
+  it('', () => {
+    const pwd = execSync('pwd').toString().replace('\n', '');
+
+    /* const checkPwd = buildPath(pwd, 'test/'); */
+    try {
+      console.log('pwd', pwd);
+      if (fs.existsSync(pwd)) {
+        console.log("Directory exists.")
+      } else {
+        console.log("Directory does not exist.")
+      }
+    } catch(e) {
+      console.log("An error occurred.")
+    }
+
+    fs.readdirSync(pwd).forEach(file => {
+      console.log('file:',file);
+    });
+
+    const evaluatorFile = buildPath(pwd, 'evaluator.js');
+    const jestOutputFile = buildPath(pwd, 'test/jest-output.json');
+    const requirementsFile = buildPath(pwd, 'test/requirements.json');
+    const resultFile = buildPath(pwd, 'test/result.json');
+
+    const expectedResultJson = {
+      github_username: 'no_actor',
+      github_repository_name: 'no_repository',
+      evaluations: [{
+        description: "Sum module",
+        grade: 3
+      }, {
+        description: "Power module",
+        grade: 3
+      }, {
+        description: "Multiply module",
+        grade: 1
+      }]
+    }
+
+    execSync(
+      `node ${evaluatorFile} ${jestOutputFile} ${requirementsFile} ${resultFile}`,
+      { stdio: 'inherit' }
+    );
+
+    const evaluationFileContent = fs.readFileSync(resultFile);
+    const resultJson = JSON.parse(evaluationFileContent);
+
+    expect(resultJson).toMatchObject(expectedResultJson);
+  });
+});
