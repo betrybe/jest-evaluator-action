ffb914581d0c5bd03934d3deeb29538a7be726ea
Gabriel de Biasi <gabriel.biasi@betrybe.com>
2022-08-23 10:12:13 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,16 @@ wait_for_url=$2
 npm install
 
 if $run_npm_start ; then
-  npm start & # Open server in background
+  npm start &> server.log & # Open server in background
   npx wait-on -t 300000 $wait_for_url # wait for server until timeout
 fi
 
 npm test -- --json --forceExit --outputFile=evaluation.json
 node /evaluator.js evaluation.json .trybe/requirements.json result.json
+
+echo "::group::Logs from API server"
+cat server.log
+echo "::endgroup::"
 
 if [ $? != 0 ]; then
   echo "Execution error"
