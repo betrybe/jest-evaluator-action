4389693f0c1f3ea64adb70dba3f4d1cde3d11be7
Igor Giamoniano <igor@Igors-MacBook-Pro.local>
2023-01-10 15:39:13 -0300

    Altera evaluationByRequirements

--- evaluator.js
+++ evaluator.js
@@ -9,40 +9,48 @@ const githubRepositoryName = process.env.GITHUB_REPOSITORY || 'no_repository';
 const jestOuputFile = fs.readFileSync(process.argv[2]);
 const { testResults } = JSON.parse(jestOuputFile);
 
-console.log("testResults ->", JSON.stringify(testResults))
+console.log('testResults ->', JSON.stringify(testResults));
 
 const requirementsFile = fs.readFileSync(process.argv[3]);
 const { requirements } = JSON.parse(requirementsFile);
 
-const evaluationsByRequirements =
-  testResults.map(({ assertionResults }) => (
-    assertionResults.map(({ ancestorTitles, status }) => ({
-      describe: ancestorTitles[ancestorTitles.length - 1],
-      status
-    }))
-  )).flat()
-    .reduce((acc, evaluation) => {
-      const status = acc[evaluation.describe];
-      const currentStatus = evaluation.status;
-      if (!status || currentStatus === 'failed') {
-        acc[evaluation.describe] = currentStatus;
-        return acc;
-      }
+const evaluationsByRequirements = testResults
+  .map(({ assertionResults }) =>
+    assertionResults.map(({ ancestorTitles, status }) => {
+      return ancestorTitles.map((describe) => ({ describe, status }));
+    })
+  )
+  .flat()
+  .reduce((acc, evaluation) => {
+    const status = acc[evaluation.describe];
+    const currentStatus = evaluation.status;
+    if (!status || currentStatus === 'failed') {
+      acc[evaluation.describe] = currentStatus;
       return acc;
-    }, {});
+    }
+    return acc;
+  }, {});
 
-const evaluations =
-    requirements.map(({ description }) => ({
-      description,
-      grade: (evaluationsByRequirements[description] === 'passed') ? CORRECT_ANSWER_GRADE : WRONG_ANSWER_GRADE
-    }));
-
-    console.log("evaluationsByRequirements ->", JSON.stringify(evaluationsByRequirements))
-
-fs.writeFileSync(process.argv[4], JSON.stringify({
-  github_username: githubUsername,
-  github_repository_name: githubRepositoryName,
-  evaluations: [...evaluations]
+const evaluations = requirements.map(({ description }) => ({
+  description,
+  grade:
+    evaluationsByRequirements[description] === 'passed'
+      ? CORRECT_ANSWER_GRADE
+      : WRONG_ANSWER_GRADE,
 }));
+
+console.log(
+  'evaluationsByRequirements ->',
+  JSON.stringify(evaluationsByRequirements)
+);
+
+fs.writeFileSync(
+  process.argv[4],
+  JSON.stringify({
+    github_username: githubUsername,
+    github_repository_name: githubRepositoryName,
+    evaluations: [...evaluations],
+  })
+);
 
 process.exit();
